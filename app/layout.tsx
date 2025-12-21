b8ee2abfdea037fdb2c1e88306c3c1c25128d331
Shubham Midgule <shubham.midgule17@gmail.com>
2025-12-21 21:59:07 +0530

    Enhance theming and layout: Introduce ThemeProvider for theme management, integrate Toaster for notifications, and update globals.css with new color variables and styles. Add a sample button to the home page for UI demonstration.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Jost } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "@/components/ui/sonner";
 
 const jostSans = Jost({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -18,7 +20,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${jostSans.className} antialiased`}>{children}</body>
+      <body className={`${jostSans.className} antialiased`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+          <Toaster richColors position="bottom-center" />
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
